60d5aa55cc3b735d0e598677b2c643d7c5df38b6
cattsas <cattsas1980@gmail.com>
2022-05-25 12:11:18 -0300

    Se agrega funcionalidad js al formulario

---
+++ TP2/EntregaFinal/assets/js/form.js
@@ -0,0 +1,22 @@
+const targets = document.querySelectorAll('[data-target]');
+const content = document.querySelectorAll('[data-content]');
+const tab = document.querySelectorAll(".pestaña");
+
+targets.forEach(target => {
+
+    target.addEventListener('click', () => {
+        content.forEach(c => {
+            c.classList.remove('activeForm')
+        })
+        tab.forEach(p => {
+            p.classList.remove('activeTab');
+
+        })
+        target.classList.add('activeTab');
+
+
+
+        const t = document.querySelector(target.dataset.target);
+        t.classList.add('activeForm');
+    })
+})
