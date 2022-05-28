a93e29153aada3437b10168fe1acbcb73f514f93
cattsas <cattsas1980@gmail.com>
2022-05-28 00:40:37 -0300

    Se agrega comportamiento js para mostrar loggin exitoso

--- TP2/EntregaFinal/assets/js/form.js
+++ TP2/EntregaFinal/assets/js/form.js
@@ -1,27 +1,44 @@
 const targets = document.querySelectorAll('[data-target]');
 const content = document.querySelectorAll('[data-content]');
 const tab = document.querySelectorAll(".pestaña");
+const log = document.querySelectorAll(".button");
+const contLoggin = document.querySelector('.regLogin');
+const succes = document.querySelector('.succesLogin');
+
+
 targets.forEach(target => {
 
     target.addEventListener('click', () => {
 
         content.forEach(c => {
-            c.classList.remove('activeForm')
+            c.classList.remove('activeForm');
 
         })
         tab.forEach(p => {
             p.classList.remove('activeTab');
 
         })
+
         target.classList.add('activeTab');
+
 
         const t = document.querySelector(target.dataset.target);
 
         t.classList.add('activeForm');
 
+    })
 
+})
+log.forEach(lg => {
 
-
+    lg.addEventListener('click', () => {
+        contLoggin.classList.remove('activeCont');
+        contLoggin.classList.add('inactiveCont');
+        succes.classList.remove('inactiveCont');
+        succes.classList.add('activeCont');
 
     })
+
+
+
 })
