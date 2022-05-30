5cec6328b6062f87c18f8e6fe418eef0e8b50deb
franbayugar <franbayu_96@hotmail.com>
2022-05-30 16:51:20 -0300

    comenzamos a desarrollar pagina busqueda

--- TP2/EntregaFinal/assets/js/adverstising.js
+++ TP2/EntregaFinal/assets/js/adverstising.js
@@ -2,7 +2,11 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
     let btnClose = document.getElementById('close_adv');
-    
+    let btnSearch = document.getElementById('btnSearch');
+
+    btnSearch.addEventListener('click', ()=>{
+        search();
+    })
     btnClose.addEventListener('click', ()=>{
         closeAdv();
     })
@@ -12,15 +16,19 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 
     window.onscroll = function() {
-        myFunction()
+        searchBarEffect()
     };
 
-    function myFunction() {
+    function searchBarEffect() {
         let barra = document.querySelector('.barra_busqueda');
         let logo = document.querySelector('.logo');
         barra.classList.toggle('lupita', window.scrollY>15);
         logo.classList.toggle('invisible', window.scrollY>15);
+  
+    }
 
-       
+    function search(){
+        let input = document.getElementById('aBuscar').value;
+        window.location.href = `search?res=${input}`;
     }
 });
