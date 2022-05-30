7e5350b69582ceea045e9064cdd916febe6ef473
Franco <franbayu_96@hotmail.com>
2022-05-30 20:52:42 -0300

    se agrega nombre de categorias a categorias

--- TP2/EntregaFinal/assets/js/category.js
+++ TP2/EntregaFinal/assets/js/category.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", ()=>{
     "use strict";
 
@@ -9,48 +8,34 @@ document.addEventListener("DOMContentLoaded", ()=>{
         let response = await fetch('https://618eb5aa50e24d0017ce13ee.mockapi.io/fran/games');
         if(response.ok){
             games = await response.json();
+            
             procesar();
+        }else{
+            console.log('error')
         }
     }
 
 let url_string = window.location.href; 
 let url = new URL(url_string);
-let id = url.searchParams.get("id");
-let selectGame = null;
-
+let category = url.searchParams.get("category");
+console.log(category);
 function procesar(){
-    for(let elem of games){
-        if(elem.id == id){
-            selectGame = elem;
-        }
-    }
 
     let categoryesGames = [];
 
     for(let elem of games){
-        if(elem.category == selectGame.category && elem.id != selectGame.id){
+        if(elem.category == category){
             categoryesGames.push(elem);
         }
     }
-    console.log(categoryesGames);
 
-    const game = new Vue({
-        el: '#game',
-        data: {
-            name: selectGame.name,
-            category: selectGame.category,
-            img: selectGame.img,
-            desarrollador: selectGame.desarrollador,
-            descripcion: selectGame.descripcion
-        }
-    })
 
     const games2 = new Vue({
         el: '#games',
         data: {
+            category: category,
             listGames: categoryesGames
         }
     })
     }
 });
-
