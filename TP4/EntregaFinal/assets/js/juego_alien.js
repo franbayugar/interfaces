809e7965b5d5ec2119c585bfa15caeb198065002
franbayugar <franbayu_96@hotmail.com>
2022-07-05 13:09:38 -0300

    agregamos opcion de elegir personaje y a haagar

--- TP4/EntregaFinal/assets/js/juego_alien.js
+++ TP4/EntregaFinal/assets/js/juego_alien.js
@@ -2,23 +2,39 @@
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
+    let personajeSelect;
     const contenedorPj = document.getElementById('character');
-    let personaje = new Personaje(contenedorPj);
-    personaje.caminar();
+
+    document.getElementById("btn_jugar").addEventListener('click',()=>{
+        crearJuego();
+    })
+
+    function crearJuego(){
+        document.getElementById('juego_menu').classList.add('oculto');
+        document.getElementById('juego_ejecucion').classList.remove('oculto');
+        let personaje = document.querySelector('input[name="personaje"]:checked').value;
+        console.log(personaje);
+        if(personaje === 'cody'){
+            personajeSelect = new Personaje(contenedorPj, 'caminando_cody', 'saltando_cody', 'deslizando_cody');
+        }else{
+            personajeSelect = new Personaje(contenedorPj, 'caminando_haagar', 'saltando_haagar', 'deslizando_haagar');
+
+        }
+    }
 
 window.onkeyup = function (event) {
        
     if (event.keyCode === 32 || event.keyCode === 38 || event.keyCode === 87) { 
         // contempla barra espaciadora flechita arriba  y la W
-        if(personaje.estado == 'caminando'){
-            personaje.saltar();
+        if(personajeSelect.estado == 'caminando'){
+            personajeSelect.saltar();
         }
      } 
 
      if (event.keyCode === 40 || event.keyCode === 83) { 
         // contempla flechita abajo  y la S
-        if(personaje.estado == 'caminando'){
-            personaje.deslizar();
+        if(personajeSelect.estado == 'caminando'){
+            personajeSelect.deslizar();
         }
      } 
 }
--- TP4/EntregaFinal/assets/js/personaje.js
+++ TP4/EntregaFinal/assets/js/personaje.js
@@ -1,8 +1,11 @@
 class Personaje {
 
-    constructor(avatar) {
+    constructor(avatar, caminata, saltando, deslizando) {
         this.contenedor = avatar;
         this.estado = 'caminando';
+        this.caminata = caminata;
+        this.saltando = saltando;
+        this.deslizando = deslizando;
         this.vivo = true;
         this.puntos = 0;
         this.caminar();
@@ -10,37 +13,37 @@ class Personaje {
     }
 
     caminar() {
-        this.contenedor.classList.add('caminando');
+        this.contenedor.classList.add(this.caminata);
     }
 
     saltar() {
-        this.contenedor.classList.replace('caminando', 'saltando');
-        this.contenedor.classList.replace('caminando', 'saltando');
+        this.contenedor.classList.replace(this.caminata, this.saltando);
+        this.contenedor.classList.replace(this.caminata, this.saltando);
         this.estado = 'saltando';
         let interval = setInterval(() => {
-            this.contenedor.classList.replace('saltando', 'caminando');
-            this.contenedor.classList.replace('saltando', 'caminando');
+            this.contenedor.classList.replace(this.saltando, this.caminata);
+            this.contenedor.classList.replace(this.saltando, this.caminata);
             this.estado = 'caminando';
 
             clearInterval(interval);
         }, 800);
-        this.contenedor.classList.remove('caminando');
-        this.contenedor.classList.add('saltando');
+        this.contenedor.classList.remove(this.caminata);
+        this.contenedor.classList.add(this.saltando);
     }
 
     deslizar() {
-        this.contenedor.classList.replace('caminando', 'deslizando');
-        this.contenedor.classList.replace('caminando', 'deslizando');
+        this.contenedor.classList.replace(this.caminata, this.deslizando);
+        this.contenedor.classList.replace(this.caminata, this.deslizando);
         this.estado = 'deslizando';
         let interval = setInterval(() => {
-            this.contenedor.classList.replace('deslizando', 'caminando');
-            this.contenedor.classList.replace('deslizando', 'caminando');
+            this.contenedor.classList.replace(this.deslizando, this.caminata);
+            this.contenedor.classList.replace(this.deslizando, this.caminata);
             this.estado = 'caminando';
 
             clearInterval(interval);
         }, 800);
-        this.contenedor.classList.remove('caminando');
-        this.contenedor.classList.add('deslizando');
+        this.contenedor.classList.remove(this.caminata);
+        this.contenedor.classList.add(this.deslizando);
     }
 
 }
