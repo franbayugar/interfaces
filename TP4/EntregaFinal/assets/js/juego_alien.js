8cfff73715cc83264472a4c099079a59abbd3c4b
cattsas <cattsas1980@gmail.com>
2022-07-06 01:44:36 -0300

    Se agregan aliens como obstáculos

--- TP4/EntregaFinal/assets/js/juego_alien.js
+++ TP4/EntregaFinal/assets/js/juego_alien.js
@@ -1,43 +1,44 @@
-
 "use strict";
 
 window.addEventListener('DOMContentLoaded', () => {
     let personajeSelect;
     const contenedorPj = document.getElementById('character');
 
-    document.getElementById("btn_jugar").addEventListener('click',()=>{
+    document.getElementById("btn_jugar").addEventListener('click', () => {
         crearJuego();
     })
 
-    function crearJuego(){
+    function crearJuego() {
         document.getElementById('juego_menu').classList.add('oculto');
         document.getElementById('juego_ejecucion').classList.remove('oculto');
         let personaje = document.querySelector('input[name="personaje"]:checked').value;
         console.log(personaje);
-        if(personaje === 'cody'){
+        if (personaje === 'cody') {
             personajeSelect = new Personaje(contenedorPj, 'caminando_cody', 'saltando_cody', 'deslizando_cody');
-        }else{
+        } else {
             personajeSelect = new Personaje(contenedorPj, 'caminando_haagar', 'saltando_haagar', 'deslizando_haagar');
 
         }
     }
 
-window.onkeyup = function (event) {
-       
-    if (event.keyCode === 32 || event.keyCode === 38 || event.keyCode === 87) { 
-        // contempla barra espaciadora flechita arriba  y la W
-        if(personajeSelect.estado == 'caminando'){
-            personajeSelect.saltar();
-        }
-     } 
+    window.onkeyup = function(event) {
 
-     if (event.keyCode === 40 || event.keyCode === 83) { 
-        // contempla flechita abajo  y la S
-        if(personajeSelect.estado == 'caminando'){
-            personajeSelect.deslizar();
+        if (event.keyCode === 32 || event.keyCode === 38 || event.keyCode === 87) {
+            // contempla barra espaciadora flechita arriba  y la W
+            if (personajeSelect.estado == 'caminando') {
+                personajeSelect.saltar();
+            }
         }
-     } 
-}
+
+        if (event.keyCode === 40 || event.keyCode === 83) {
+            // contempla flechita abajo  y la S
+            if (personajeSelect.estado == 'caminando') {
+                personajeSelect.deslizar();
+            }
+        }
+    }
+
+
 
 
 });
